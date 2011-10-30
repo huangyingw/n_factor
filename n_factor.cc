b465478561b8e61b690cb6a16651f3d639b806d2
huangyingw@linux_programming <huangyingw@gmail.com>
2011-10-30 17:46:13 +0800

    n

--- n_factor.cc
+++ n_factor.cc
@@ -4,48 +4,18 @@
 
 using namespace std;
 
-int f(int a[ ], int len)
+int isNFactored(int value, int n)
 {
-  //X = the sum of the odd numbers in the array
-  //Y = the sum of the even numbers
-  int X=0,Y=0;
-  for (int i=0;i<len;i++)
-  {
-    if (0== a[i]%2)
-      Y+=a[i];
-    else
-      X+=a[i];
-  }
-  return X - Y;
+  return 0;
 }
 
 int main() 
 {
-  int a[] ={1} ;//1
-  cout<<f(a,sizeof(a)/sizeof(int))<<endl;
-
-  int b[]={1, 2};//-1
-  cout<<f(b,sizeof(b)/sizeof(int))<<endl;
-
-  int c[]={1, 2, 3};//2
-  cout<<f(c,sizeof(c)/sizeof(int))<<endl;
-
-  int d[]={1, 2, 3, 4};//-2
-  cout<<f(d,sizeof(d)/sizeof(int))<<endl;
-
-  int e[]={3, 3, 4, 4};//-2
-  cout<<f(e,sizeof(e)/sizeof(int))<<endl;
-
-  int h[]={3, 2, 3, 4};//0
-  cout<<f(h,sizeof(h)/sizeof(int))<<endl;
-
-  int g[]={4, 1, 2, 3};//-2
-  cout<<f(g,sizeof(g)/sizeof(int))<<endl;
-
-  int j[]={1, 1};//2
-  cout<<f(j,sizeof(j)/sizeof(int))<<endl;
-
-  int i[]={};//0
-  cout<<f(i,sizeof(i)/sizeof(int))<<endl;
+  cout<<isNFactored(14,2)<<endl;//1 14 has two isNFactoredactors, 2 and 7
+  cout<<isNFactored(18,5)<<endl;//0 18 does not have 5 isNFactoredactors, it only has isNFactoredour (2, 3, 6 and 9)
+  cout<<isNFactored(25,2)<<endl;//0 25 = 5*5 but we are only interested in distinct isNFactoredactors so the 5 is only counted once.
+  cout<<isNFactored(36,7)<<endl;//1 36 has seven isNFactoredactors (2, 3, 4, 6, 9, 12, 18). Note that 6 is counted only once.
+  cout<<isNFactored(7,0)<<endl;//1 all prime numbers are 0-isNFactoredactored
+  cout<<isNFactored(-5,3)<<endl;//-5 anything 0 the value must be greated than 0
   return 0;
 }
